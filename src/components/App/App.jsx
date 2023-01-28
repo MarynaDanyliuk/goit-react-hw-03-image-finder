8a57ef7e8c0e8e760545c97232c9fcb8d3148f0d
Mary Danyliuk <marydanyliuk@gmail.com>
2023-01-28 14:19:43 +0200

    upd Modal toggle ESC-close & overlay-close

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Button } from '../Button/Button';
 import { LoaderWatch } from '../Loader/Loader';
-import { Modal } from '../Modal/Modal';
+import Modal from '../Modal/Modal';
 
 // import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
@@ -23,7 +23,7 @@ export class App extends React.Component {
     images: [],
     loading: false,
     page: 1,
-    showModal: false,
+    showModal: true,
   };
 
   componentDidMount() {
@@ -34,40 +34,6 @@ export class App extends React.Component {
       this.onToggleModal();
     });
   }
-
-  // onHandleSubmit = ({ query }) => {
-  //   this.setState({
-  //     query: query,
-  //     // page: 1,
-  //     // images: [],
-  //     // loading: true,
-  //   });
-
-  // console.log(`до запроса наш объект`, this.state);
-
-  // getImagesApiService.resetPage();
-
-  // if (query.trim() === '') {
-  //   this.setState({
-  //     images: [],
-  //   });
-  //   return;
-  // }
-
-  // getImagesApiService
-  //   .fetchImages(query)
-  //   .then(data => {
-  //     this.setState({ images: data.hits });
-  //   })
-  //   .catch(error => {
-  //     console.log('Error');
-  //   })
-  //   .finally(() => {
-  //     this.setState({ loading: false });
-  //   });
-
-  // console.log(`После запроса, если все ок - наш объект`, getImagesApiService);
-  // };
 
   componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
@@ -83,6 +49,22 @@ export class App extends React.Component {
       this.fetchImages();
     }
   }
+
+  componentWillUnmount() {
+    // window.addEventListener('keydown', event => {
+    //   if (event.code === 'Escape') {
+    //     console.log(' нажали ESC, нужно закрыть модалку', event.currentTarget);
+    //     this.onToggleModal();
+    //   }
+    // });
+  }
+
+  onToggleModal = event => {
+    console.log('кликнули overlay модального окна');
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
 
   async fetchImages() {
     try {
@@ -106,109 +88,23 @@ export class App extends React.Component {
     // console.log(`до запроса наш объект`, this.state);
   };
 
-  // [...images, ...data.hits]
-
-  // componentDidUpdate(_, prevState) {
-  //   console.log(`до запроса наш объект`, this.state);
-
-  //   console.log('prevState.page:', prevState.page);
-  //   console.log('this.state.page:', this.state.page);
-
-  //   console.log('prevState.query:', prevState.query);
-  //   console.log('this.state.query:', this.state.query);
-
-  //   if (
-  //     prevState.page !== this.state.page ||
-  //     prevState.query !== this.state.query
-  //   ) {
-  //     this.fetchImages(this.state.query);
-
-  //     // this.resetPage();
-  //     // getImagesApiService.resetPage();
-  //   }
-  // }
-
-  // fetchImages = query => {
-  //   query = this.state.query;
-
-  //   if (query.trim() === '') {
-  //     this.setState({
-  //       images: [],
-  //     });
-  //     return;
-  //   }
-
-  //   const API_KEY = '31808257-b1d1bead71ab6681d9f118ecf';
-  //   const BASE_URL = 'https://pixabay.com/api/';
-
-  //   return fetch(
-  //     `${BASE_URL}?q=${query}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  //   )
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({ images: data.hits });
-  //       // console.log(`После запроса, если все ок - наш объект`, this.state);
-  //     })
-  //     .catch(error => {
-  //       console.log('Error');
-  //     })
-  //     .finally(() => {
-  //       this.setState({ loading: false });
-  //     });
-  // };
-
   onLoadMore = () => {
     this.setState(prevState => ({ page: prevState.page + 1 }));
     // console.log(`После запроса, если все ок - наш объект`, this.state);
   };
-
-  componentWillUnmount() {
-    window.addEventListener('keydown', event => {
-      if (event.code === 'Escape') {
-        console.log(' нажали ESC, нужно закрыть модалку', event.currentTarget);
-        this.onToggleModal();
-      }
-    });
-  }
-
-  resetPage() {
-    this.setState({
-      page: 1,
-    });
-  }
-
-  // onHandelClick = () => {
-  //   console.log('кликнули на Load more');
-
-  //   getImagesApiService.incrementPage();
-  //   console.log(`После запроса, если все ок - наш объект`, getImagesApiService);
-  // };
-
-  // onToggleModal = event => {
-  //   console.log('кликнули Toggle модального окна');
-  //   this.setState(({ showModal }) => ({
-  //     showModal: !showModal,
-  //   }));
-  // };
 
   render() {
     const { images, loading, showModal } = this.state;
 
     return (
       <Container>
-        <Searchbar
-          onSubmit={this.searchImages}
-          // onSubmit={this.onHandleSubmit}
-          // onClick={this.onToggleModal}
-        />
+        <Searchbar onSubmit={this.searchImages} />
         {loading && <LoaderWatch />}
         {images && <ImageGallery images={images} />}
-
         {images.length > 0 && <Button handelClick={this.onLoadMore} />}
 
-        {/* {images.length > 0 && <Button handelClick={this.onHandelClick} />} */}
         {showModal && (
-          <Modal handleToggle={this.onToggleModal}>
+          <Modal handleToggle={() => this.onToggleModal()}>
             <h1>Hello I'm MODALKA</h1>
           </Modal>
         )}
@@ -216,6 +112,101 @@ export class App extends React.Component {
     );
   }
 }
+//  ___________________________________________
+// onHandleSubmit = ({ query }) => {
+//   this.setState({
+//     query: query,
+//     // page: 1,
+//     // images: [],
+//     // loading: true,
+//   });
+
+// console.log(`до запроса наш объект`, this.state);
+
+// getImagesApiService.resetPage();
+
+// if (query.trim() === '') {
+//   this.setState({
+//     images: [],
+//   });
+//   return;
+// }
+
+// getImagesApiService
+//   .fetchImages(query)
+//   .then(data => {
+//     this.setState({ images: data.hits });
+//   })
+//   .catch(error => {
+//     console.log('Error');
+//   })
+//   .finally(() => {
+//     this.setState({ loading: false });
+//   });
+
+// console.log(`После запроса, если все ок - наш объект`, getImagesApiService);
+// };
+// ________________________________________
+
+// [...images, ...data.hits]
+
+// componentDidUpdate(_, prevState) {
+//   console.log(`до запроса наш объект`, this.state);
+
+//   console.log('prevState.page:', prevState.page);
+//   console.log('this.state.page:', this.state.page);
+
+//   console.log('prevState.query:', prevState.query);
+//   console.log('this.state.query:', this.state.query);
+
+//   if (
+//     prevState.page !== this.state.page ||
+//     prevState.query !== this.state.query
+//   ) {
+//     this.fetchImages(this.state.query);
+
+//     // this.resetPage();
+//     // getImagesApiService.resetPage();
+//   }
+// }
+
+// fetchImages = query => {
+//   query = this.state.query;
+
+//   if (query.trim() === '') {
+//     this.setState({
+//       images: [],
+//     });
+//     return;
+//   }
+
+//   const API_KEY = '31808257-b1d1bead71ab6681d9f118ecf';
+//   const BASE_URL = 'https://pixabay.com/api/';
+
+//   return fetch(
+//     `${BASE_URL}?q=${query}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//   )
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({ images: data.hits });
+//       // console.log(`После запроса, если все ок - наш объект`, this.state);
+//     })
+//     .catch(error => {
+//       console.log('Error');
+//     })
+//     .finally(() => {
+//       this.setState({ loading: false });
+//     });
+// };
+
+// onHandelClick = () => {
+//   console.log('кликнули на Load more');
+
+//   getImagesApiService.incrementPage();
+//   console.log(`После запроса, если все ок - наш объект`, getImagesApiService);
+// };
+
+// _____________________________________________________________
 
 /* <ImageGalleryItem images={images[0]} handleToggle={this.onToggleModal} />; */
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import { createPortal } from 'react-dom';
 
 // import PropTypes from 'prop-types';
@@ -6,11 +8,40 @@ import { ModalOverlay, ModalView } from '../Modal/Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export const Modal = ({ children, handleToggle }) => {
-  return createPortal(
-    <ModalOverlay onClick={handleToggle}>
-      <ModalView>{children}</ModalView>
-    </ModalOverlay>,
-    modalRoot
-  );
-};
+class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleToggle);
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('keydown', this.handleToggle);
+  }
+
+  handleToggle = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === 'Escape') {
+      this.props.handleToggle(this.state);
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+
+    return createPortal(
+      <ModalOverlay onClick={this.handleToggle}>
+        <ModalView>{children}</ModalView>
+      </ModalOverlay>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
+
+// export const Modal = ({ children, handleToggle }) => {
+// return createPortal(
+//   <ModalOverlay onClick={handleToggle}>
+//     <ModalView>{children}</ModalView>
+//   </ModalOverlay>,
+//   modalRoot
+// );
+// };
